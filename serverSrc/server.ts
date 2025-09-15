2d0e2c89574552c0d2991237314349d587b046e8
David Andersson <lejonmanen@gmail.com>
2025-09-15 10:09:03 +0200

    static + DELETE /movies/id

--- serverSrc/server.ts
+++ serverSrc/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import type { Express, Response } from 'express'
+import type { Express, Request, Response } from 'express'
 import { badMovies } from './data/movies.js'
 import { Movie } from './data/types.js'
 
@@ -9,11 +9,27 @@ const port = 1337
 
 // Middleware
 app.use('/', cors())
+app.use('/', express.static('./dist/'))
 
 // Endpoints
-app.get('/movies', (req, res: Response<Movie[]>) => {
+app.get('/api/movies', (req, res: Response<Movie[]>) => {
 	console.log('GET /movies')
 	res.send(badMovies)  // status: 200 OK
+})
+
+interface IdParam {
+	id: string;
+}
+app.delete('/api/movies/:id', (req: Request<IdParam>, res: Response<void>) => {
+	const id: string = req.params.id
+	const index: number = badMovies.findIndex(movie => movie.id === id)
+	
+	if( index === -1 ) {
+		res.sendStatus(404)
+		return
+	}
+	badMovies.splice(index, 1)
+	res.sendStatus(204)
 })
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import type { Movie } from './data/types';
 
 type EventCallback = () => void;
+type EventCallbackWithId = (id: string) => void;
 
 const App = () => {
 	const [movies, setMovies] = useState<null | Movie[]>(null)
@@ -10,7 +11,7 @@ const App = () => {
 	const handleInspireClick: EventCallback = async () => {
 		// OBS! Vi skriver URL på ett annat sätt - senare
 		// Felhantering: try/catch, kontrollera statuskod
-		const response: Response = await fetch('http://localhost:1337/movies', {
+		const response: Response = await fetch('/api/movies', {
 			method: 'GET'
 		})
 		if( response.status !== 200 ) {
@@ -21,6 +22,18 @@ const App = () => {
 		// TODO: validera att vi får korrekt data (lista med Movie objekt)
 		setMovies(data)
 	}
+	const handleDelete: EventCallbackWithId = async id => {
+		const response = await fetch('/api/movies/' + id, {
+			method: 'DELETE'
+		})
+		console.log('handleDelete, response from server = ', response.status)
+		if( response.status === 204 ) {
+			// todo: visa en uppdatering för användaren "allt har lyckats"
+			handleInspireClick()  // hämta listan från servern igen
+		} else {
+			// todo: visa felmeddelande för användaren
+		}
+	}
 
 	return (
 		<div className="app">
@@ -28,7 +41,9 @@ const App = () => {
 			<button onClick={handleInspireClick}> Inspire me! </button>
 			<div className="movies">
 				{movies && movies.map(movie => (
-					<p key={movie.id}> {movie.title} - {movie.premiere} </p>
+					<p key={movie.id}> {movie.title} - {movie.premiere}
+					<button onClick={() => handleDelete(movie.id)}> Delete </button>
+					</p>
 				))}
 			</div>
 		</div>
